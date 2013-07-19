40ad46146e22c040566806e3bca012d4ef565ca0
ali.muzaffar <atgheb@gmail.com>
2013-07-19 23:49:39 +1000

    initial commit

---
+++ GestureBuilderActivity/gen/com/android/gesture/builder/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.android.gesture.builder;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ GesturesTutorialStep01/gen/com/alimuzaffar/tutorial/gestures01/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.alimuzaffar.tutorial.gestures01;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
